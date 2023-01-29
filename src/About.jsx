8e3c26b34efea9c9f5dac11ae92f3ddc33e92368
Pawit <pawitprogrammer@gmail.com>
2023-01-30 00:23:09 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+import './About.css'
+
+function About() {
+    return (
+        <div>
+           <h3 className='About-txt'>This is a Web for a Code Share  And Reatime Code Editor 🏴 and Also you can make code And Publish ↟ <br /> Also you can test your code In Cloud 🌤 or This Project made by <a className='abouts' href="https://youtube.com/@PawitSahare">Pawit Anand Sahare</a></h3>
+        </div>
+    )
+};
+
+export default About
---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import About from './About'
+function App() {
+  return (
+    <div>
+      <h1 className='code'>Code.</h1>
+      <p className='hunt'>Huntinner  !</p>
+      {/* Arrow */}
+
+      <span className='material-symbols-outlined'>
+        arrow_downward
+</span>
+
+    <div className='scroll-bar'>
+    <button>
+    Get code<a href='https://repo-share-pawit.web.app'>. here</a>
+    </button>
+   <div>
+    <br />
+   <button>Online Code Editor ⚡</button>
+   </div>
+  
+</div>
+<div>
+   <h3 className='about-txt'>About:this</h3>
+   <About/>
+</div>
+   
+
+
+    </div>
+  )
+
+}
+
+export default App
